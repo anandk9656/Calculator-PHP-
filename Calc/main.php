67a105b2e503846a2a7ae5dc1f714d52f369e6b1
Anand Kumar <anandk9656@gmail.com>
2017-09-11 10:53:26 +0530

    Update main.php

--- Calc/main.php
+++ Calc/main.php
@@ -1,33 +1,37 @@
-// Cookies in calculator
 <?php
-
+    // Cookies in calculator for storing input (4+9) and output (13) on system.
     if(isset($_GET['Answer']))
     {
         if(!empty($_GET['temp']))
             {
                 eval('$res= ' . $_GET['temp'] . ';');
                
+                // Set cookies if cookie is empty.
                 if(empty($_COOKIE['history']))
                 {
                     setcookie('history',$_GET['temp'].!empty($_GET['num'])."=".$res.",", time()+86400);
                 }
+                // concatenate the cookie is it is not empty.
                 else
                 {
                     setcookie('history',$_GET['temp'].!empty($_GET['num'])."=".$res.",".$_COOKIE['history'], time()+86400);
                 }
            }
     }
-    
+    // Printing the last 10 history .
     if(isset($_COOKIE['history']))
     {
         foreach($_COOKIE as $value)
         {       
             $new = explode(",",$value);
         }
-        echo '<pre>';
-        echo "<h3> Total History </h3>";
-        print_r($new);
-        echo '</pre>';
+        
+/** Uncomment below code for having full history. 
+*        echo '<pre>';
+*       echo "<h3> Total History </h3>";
+*        print_r($new);
+*       echo '</pre>';
+**/
         echo "<h3> Last Ten History  </h3>";
         echo current($new);
         echo "<br>";
