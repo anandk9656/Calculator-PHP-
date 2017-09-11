b53abd4b78ebd5ab678a2b4feec5d9b1b3460b1d
Anand Kumar <anandk9656@gmail.com>
2017-09-11 10:47:45 +0530

    Update hidden.php

--- Calc/hidden.php
+++ Calc/hidden.php
@@ -1,16 +1,17 @@
-// Code for handling last input value and current input value.
+
 <?php
 
+// Code when any number is pressed from the calculator
 if(isset($_GET['num']))
    {         
       echo $_GET['temp'].$_GET['num'];
    }
-
+// Code when any backspace is pressed from the calculator
 else if(isset($_GET['Backspace']))
    {
       echo substr($_GET['input'],0,-1);
    }
-   
+ // Code when any = is pressed from the calculator  
 elseif(isset ($_GET['Answer']))
    {
       echo $res;
